604b4942b766ad7700e6923814393d090b9786cc
wonguwon <wonguwon25@gmail.com>
2024-11-29 00:02:36 +0900

    add - select order api

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //해당 어노테이션과 같거나 이하 패키지의 모든 객체를 스캔한다.
 @SpringBootApplication
@@ -11,4 +13,11 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5JakartaModule hibernate5Module() {
+		Hibernate5JakartaModule hibernate5Module = new Hibernate5JakartaModule();
+		//강제로 지연 로딩된 데이터를 모두 가져오게 설정
+		//hibernate5Module.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
+	}
 }
